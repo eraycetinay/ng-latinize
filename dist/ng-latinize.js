955b4b6ca757d85e848aa09b598907f545ea091c
Eray <eraycetinay@gmail.com>
2017-10-10 21:36:30 +0300

    initial commit

---
+++ dist/ng-latinize.js
@@ -0,0 +1,38 @@
+(function (root, factory) {
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    define(['angular'], factory);
+  } else if (typeof module !== 'undefined' && typeof module.exports === 'object') {
+    module.exports = factory(require('angular'));
+  } else {
+    return factory(root.angular);
+  }
+}(this, function (angular) {
+  'use strict';
+  var moduleName = 'ngLatinize';
+  var mod = angular.module(moduleName, []); ;
+  mod.directive('latinize', function() {
+    return {
+      require: 'ngModel',
+      scope:{
+      	capitalize: '='
+      },
+      link: function(scope, element, attrs, modelCtrl) {
+        var latinizedvalue = function(inputValue) {
+          if (inputValue == undefined) inputValue = '';
+          var latinized = latinize(inputValue);
+          if(scope.capitalize) {
+          	latinized = latinized.toUpperCase();
+          }
+          if (latinized !== inputValue) {
+            modelCtrl.$setViewValue(latinized);
+            modelCtrl.$render();
+          }
+          return latinized;
+        }
+        modelCtrl.$parsers.push(latinizedvalue);
+        latinizedvalue(scope[attrs.ngModel]);
+      } }
+  });
+  return moduleName;
+}));
